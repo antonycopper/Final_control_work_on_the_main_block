f979385b623592e0807a28a4d7a5aa06d0e70ddd
Anton <antonycopper22@gmail.com>
2023-05-03 22:34:18 +0700

    Написали программу на языке C#, для выполнения задания под номером 4

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача: Написать программу, которая из имеющегося массива
+строк формирует новый массив из строк, 
+длина которых меньше, либо равна 3 символам. 
+Первоначальный массив можно ввести с клавиатуры, 
+либо задать на старте выполнения алгоритма. 
+При решении не рекомендуется пользоваться коллекциями, 
+лучше обойтись исключительно массивами.
+
+Пример ввода:
+
+Введите массив строк через пробел:
+abcd ef g hijkl mn
+
+
+Пример вывода:
+
+Новый массив строк:
+ef g mn
+
+Пример кода на C#:
+*/
+
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // ввод исходного массива строк с клавиатуры
+        Console.WriteLine("Введите массив строк через пробел:");
+        string[] array = Console.ReadLine().Split();
+
+        // создание нового массива с длиной строки <= 3
+        string[] newArray = new string[array.Length];
+        int count = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i].Length <= 3)
+            {
+                newArray[count] = array[i];
+                count++;
+            }
+        }
+
+        // вывод нового массива на экран
+        Console.WriteLine("\nНовый массив строк:");
+        for (int i = 0; i < count; i++)
+        {
+            Console.Write(newArray[i] + " ");
+        }
+    }
+}
